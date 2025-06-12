adc6bc59d566eb302b91e29e59d183fd9f4ae3c4
Nwouatou Yvanie Noelle <nwouatouyvanienoelle@gmail.com>
2025-06-12 13:11:02 +0100

    giant website home page

---
+++ script.js
@@ -0,0 +1,19 @@
+let lastScrollTop = 0;
+const topNavbar = document.getElementById("top-navbar");
+const mainNavbar = document.querySelector(".main-navbar");
+
+window.addEventListener("scroll", function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  // Scroll Down - Hide Top Navbar and move Main Navbar up
+  if (currentScroll > lastScrollTop) {
+    topNavbar.classList.add("hidden");
+    mainNavbar.classList.add("hidden");
+  } else {
+    // Scroll Up - Show Top Navbar and move Main Navbar back
+    topNavbar.classList.remove("hidden");
+    mainNavbar.classList.remove("hidden");
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+});
